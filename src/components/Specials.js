fdfb6f81d9ab3fefabc36b3f51572494eb2bd3fd
risheq <mohd.risheq@gmail.com>
2023-05-31 10:30:20 +0300

    Responsive HeroSection

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,8 +1,14 @@
+import { NavLink } from 'react-router-dom'
+import '../styles/Specials.css'
 
 const Specials = () => {
   return (
-    <>
-    </>
+    <div className='container'>
+    <div className='specials-header'>
+      <h1>Specials</h1>
+      <NavLink to="/menu"><button className="btn">Online Menu</button></NavLink>
+    </div>
+    </div>
   );
 }
 export default Specials;
