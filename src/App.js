96ecfc6742e61697a5be94e538bed7390bec912c
risheq <mohd.risheq@gmail.com>
2023-05-31 09:24:47 +0300

    Full routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import Home from "./routes/Home";
+import About from "./routes/About";
+import Menu from "./routes/Menu";
 import Reservations from "./routes/Reservations";
+import Order from "./routes/Order";
+import Login from "./routes/Login";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element = {<Home/>}/>
+        <Route path="/about" element ={<About/>}/>
+        <Route path="/menu" element ={<Menu/>}/>
         <Route path="/reservations" element ={<Reservations/>}/>
+        <Route path="/order" element ={<Order/>}/>
+        <Route path="/login" element ={<Login/>}/>
       </Routes>
     </>
   );
---
+++ src/routes/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Login = () => {
+    return (
+        <>
+          <Navbar/>
+        </>
+    )
+}
+export default Login
