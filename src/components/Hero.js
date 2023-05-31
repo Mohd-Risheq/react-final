a8b9aeca2874a764999b0a4cca5bf9e95b64a97e
risheq <mohd.risheq@gmail.com>
2023-05-31 09:17:00 +0300

    Starting hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,8 +3,21 @@ import heroImage from "../images/restauranfood.jpg"
 
 const Hero = () => {
   return (
-    <>
-    </>
+    <div className='bg'>
+      <div className='container'>
+        <div className='left-section'>
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+          <NavLink to="/reservations"><button className="btn">Reserve a Table</button></NavLink>
+        </div>
+        <div className='right-section'>
+          <div className='image-box'>
+            <img src={heroImage} alt='Hero Image'/>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 export default Hero;
